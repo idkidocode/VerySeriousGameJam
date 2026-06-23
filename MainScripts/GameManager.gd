e891880f86570a69981667389f3fc4c9fad8beed
idkidocode <lucasgamer20111@outlook.com>
2026-06-24 02:58:40 +1000

    punching

--- MainScripts/GameManager.gd
+++ MainScripts/GameManager.gd
@@ -3,7 +3,7 @@ extends Node
 
 var GunStats = {
 	"Damage" = 10,
-	"Reload" = 1
+	"Reload" = 0.8
 }
 
 # Called when the node enters the scene tree for the first time.
--- Script/player_movement.gd
+++ Script/player_movement.gd
@@ -9,6 +9,8 @@ var Accel = 1800
 var Friction = 1300
 var isSprinting = false
 
+
+
 #// Health \\#
 @export var MaxHealth: float = 100.0
 var Health: float
@@ -18,32 +20,30 @@ var invincible: bool = false
 @onready var weapon_anchor: Node2D = $WeaponAnchor
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-
-
-
-@onready var fist_anchor: Area2D = $FistAnchor
-@onready var ray_cast_2d: RayCast2D = $FistAnchor/RayCast2D
 var canAttack = true
+var CurrentWeapon: Node2D
 
 func _ready() -> void:
 	SetSpeed = SPEED
 	Health = MaxHealth
+	CurrentWeapon = weapon_anchor.get_child(0)
+	#make the punch not loop or it screws up
+	animated_sprite_2d.sprite_frames.set_animation_loop("Punch01", false)
 
 func _physics_process(delta: float) -> void:
 	#// Movement \\#
 	var dir: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
-
-	
-	if dir == Vector2(0, 0):
-		animated_sprite_2d.play("idle")
-	elif dir != Vector2(0, 0):
-		if isSprinting:
-			animated_sprite_2d.play("Running")
+	if canAttack:
+		if dir == Vector2(0, 0):
+			animated_sprite_2d.play("idle")
+		elif dir != Vector2(0, 0):
+			if isSprinting:
+				animated_sprite_2d.play("Running")
+			else:
+				animated_sprite_2d.play("Walking")
 		else:
-			animated_sprite_2d.play("Walking")
-	else:
-		animated_sprite_2d.stop()
+			animated_sprite_2d.stop()
 		
 	if dir != Vector2.ZERO:
 		velocity = velocity.move_toward(dir * SPEED, Accel * delta)
@@ -53,7 +53,7 @@ func _physics_process(delta: float) -> void:
 	var MousePos = get_global_mouse_position()
 	var MouseAngle = global_position.angle_to_point(MousePos)
 	
-	fist_anchor.rotation = MouseAngle
+	weapon_anchor.rotation = MouseAngle
 	
 	if MousePos.x > global_position.x:
 		animated_sprite_2d.flip_h = true
@@ -70,30 +70,18 @@ func _physics_process(delta: float) -> void:
 	
 	move_and_slide()
 	#// Attack Funcitons \\#
-	
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) && canAttack:
-		Punch()
-	
-
-func Punch() -> void:
-	canAttack = false
-	print(canAttack)
-	#// The collsion \\#
-	if ray_cast_2d.is_colliding():
-		var collider = ray_cast_2d.get_collider()
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) && canAttack && CurrentWeapon != null :
+		if canAttack:
+			if CurrentWeapon.name == "Fist":
+				animated_sprite_2d.stop()
+				animated_sprite_2d.play("Punch01")
+			CurrentWeapon.Attack()
 		
-		if collider.is_in_group("Enemy"):
-			if collider.has_method("TakeDamage"):
-				var PlayerDamage = GameManager.GunStats["Damage"]
-				var Reload = GameManager.GunStats["Reload"]
-				
-				collider.TakeDamage(PlayerDamage)
-				
-				await get_tree().create_timer(Reload).timeout
+		canAttack = false
+		await get_tree().create_timer(GameManager.GunStats["Reload"]).timeout
+		canAttack = true
 
-	canAttack = true
-
-#// Health \\#
+#// Health \\#sss
 func TakeDamage(amount: float) -> void:
 	if invincible:
 		return
